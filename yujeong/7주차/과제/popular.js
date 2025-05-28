068ec6f4a6c8328ddb91e363c34df7dbaa0a5a7b
Yoojeong <91018849+syj02719@users.noreply.github.com>
2025-05-28 17:01:09 +0900

    Update popular.js

--- yujeong/7주차/과제/popular.js
+++ yujeong/7주차/과제/popular.js
@@ -1,9 +1,9 @@
 const options = {
-  method: "GET",
+  method: 'GET',
   headers: {
-    accept: "application/json",
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9....", // 생략
-  },
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTBmM2U2MGUwMDdhNDNhNWUwZTY4M2QxMjFkMDRhOCIsIm5iZiI6MTc0Mjk1NDc4Mi44MTUsInN1YiI6IjY3ZTM2MTFlZWM5OGJmMGEwZDc1ZmZhYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LL7SqBIef0eKPzgJAvH5KNr8oYGQfPne6jIS-B241zY'
+  }
 };
 
 fetch(
